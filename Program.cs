65a1c7c16748128ed8248e1d80e1402fd82c264c
Alex <tanko77@rambler.ru>
2022-09-18 12:50:00 +0300

    2

--- Program.cs
+++ Program.cs
@@ -3,38 +3,80 @@
 // чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+// Console.Clear();
+// Console.Write("Задайте размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Наш массив: ");
+// PrintArray(numbers);
+// int count = 0;
+
+// for (int z = 0; z < numbers.Length; z++)
+// if (numbers[z] % 2 == 0)
+// count++;
+
+// Console.WriteLine($"В массиве чётных чисел {count}" );
+
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(100,1000);
+//     }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[");
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         if (i!=(numbers.Length-1))
+//               Console.Write($"{numbers[i]}, ");
+//         else 
+//               Console.Write($"{numbers[i]}");
+//     }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
 Console.Clear();
 Console.Write("Задайте размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
+Console.WriteLine("Наш массив:");
 PrintArray(numbers);
-int count = 0;
+int sum = 0;
 
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
-count++;
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
 
-Console.WriteLine($"В массиве чётных чисел {count}" );
+    Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
 
 void FillArrayRandomNumbers(int[] numbers)
 {
     for(int i = 0; i < numbers.Length; i++)
-    {
-        numbers[i] = new Random().Next(100,1000);
-    }
+        {
+            numbers[i] = new Random().Next(-100,100);
+        }
 }
 void PrintArray(int[] numbers)
 {
     Console.Write("[");
     for(int i = 0; i < numbers.Length; i++)
-    {
-        if (i!=(numbers.Length-1))
+        {
+            if (i!=(numbers.Length-1))
               Console.Write($"{numbers[i]}, ");
-        else 
+            else 
               Console.Write($"{numbers[i]}");
-    }
+        }
     Console.Write("]");
     Console.WriteLine();
 }
